db6eb7a19b34c199952f421b7b4cdafcb8f6bb2b
MaTtiaa01 <mattia2001@live.it>
2022-12-16 15:33:35 +0100

    add header and jumbotron

---
+++ comics/resources/views/partials/header.blade.php
@@ -0,0 +1,25 @@
+<header>
+    <div class="header_top">
+
+    </div>
+    <div class="container">
+        <div class="header_bottom d-flex mt-5">
+            <div class="logo">
+                <img src=" {{ Vite::asset('resources/img/dc-logo.png')}} " alt="">
+            </div>
+            <nav class="nav justify-content-center aling-items-center">
+                <a class="nav-link" href="#">Active link</a>
+                <a class="nav-link" href="#">Link</a>
+                <a class="nav-link " href="#">Disabled link</a>
+                <a class="nav-link" href="#">Active link</a>
+                <a class="nav-link" href="#">Link</a>
+                <a class="nav-link " href="#">Disabled link</a>
+                <a class="nav-link" href="#">Active link</a>
+                <a class="nav-link" href="#">Link</a>
+                <a class="nav-link " href="#">Disabled link</a>
+                <a class="nav-link " href="#">Disabled link</a>
+                <input type="search" name="search" id="search">
+            </nav>
+        </div>
+    </div>
+</header>
