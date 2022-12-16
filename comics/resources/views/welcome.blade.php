af4ac5bcaeb30e6a4af01626d3dd351cdbb52f9a
MaTtiaa01 <mattia2001@live.it>
2022-12-16 15:45:51 +0100

    print comics card in page

--- comics/resources/views/welcome.blade.php
+++ comics/resources/views/welcome.blade.php
@@ -4,7 +4,29 @@
 
 <main>
     <section class="jumbotron">
+    </section>
 
+    <section class="comics_card ">
+        <div class="container">
+            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4 row-cols-lg-6 g-4">
+                @forelse($comics as $comic)
+                <div class="col">
+                    <div class="card">
+                        <div class="card-head">
+                            <img src="{{$comic['thumb']}}" alt="">
+                        </div>
+                        <div class="card-body">
+                            <h5>{{$comic['series']}}</h5>
+                        </div>
+                    </div>
+                </div>
+                @empty
+                <div class="col">
+                    <p>No comics to show yet!</p>
+                </div>
+                @endforelse
+            </div>
+        </div>
     </section>
 
 </main>
--- comics/routes/web.php
+++ comics/routes/web.php
@@ -15,5 +15,5 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     $comics = config('db.comics');
-    return view('welcome', $comics);
+    return view('welcome', compact('comics'));
 })->name("home");
